d0acd0b86ea89f629546d272e27a9fe879d7df22
VinoliaMoekwa <moe23001@byui.edu>
2025-11-19 19:43:23 +0200

    Added Gravity Forms after submission hook sending data Webhook.site my plugin

--- my-custom-plugin.php
+++ my-custom-plugin.php
@@ -15,37 +15,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Enqueue custom CSS
 function my_custom_plugin_styles() {
-    wp_enqueue_style( 'my-custom-plugin', plugin_dir_url(__FILE__) . 'css/my-custom-plugin.css', array(), '1.0' );
+	wp_enqueue_style( 'my-custom-plugin', plugin_dir_url(__FILE__) . 'css/my-custom-plugin.css', array(), '1.0' );
 }
 add_action('wp_enqueue_scripts', 'my_custom_plugin_styles');
 
 // Enqueue custom JS
 function my_custom_plugin_scripts() {
-    wp_enqueue_script( 'my-custom-script', plugin_dir_url(__FILE__) . 'js/my-custom-plugin.js', array('jquery'), '1.0', true );
+	wp_enqueue_script( 'my-custom-script', plugin_dir_url(__FILE__) . 'js/my-custom-plugin.js', array('jquery'), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'my_custom_plugin_scripts' );
 
 //Test change for pull request
 
 //Hook into Gravity Forms after submission
-add_action('gform_after_submission', 'my_custom_plugin_after_submission' 10,2);
+add_action('gform_after_submission', 'my_custom_plugin_after_submission', 10,2);
 
 function my_custom_plugin_after_submission($entry ,$form ) {
 	//Example :send data to an external site via POST
-	$data = array (
-		'name' => rgar($entry, '1'), //Field ID 1
-		'email' => rgar($entry, '2'), //Field ID 2
-		'describe the theme and occasion for the cake' =>rgar($entry, '3'), //Field ID 3
+	$data = array(
+		'name'               => rgar( $entry, '1' ), //Field ID 1
+		'email'              => rgar( $entry, '2' ), //Field ID 2
+		'theme_and_occasion' => rgar( $entry, '3' ), //Field ID 3
 	);
 
 	$webhook_url = 'https://webhook.site/e544100e-3dce-4827-9d37-14811098b764';
 
 
-$args = array(
-	'body' => json_encode($data),
-	'headers' => array('Content-Type' => 'application/json'),
-	'timeout' => 15
-);
+	$args = array(
+		'body'    => json_encode( $data ),
+		'headers' => array( 'Content-Type' => 'application/json' ),
+		'timeout' => 15
+	);
 
-wp_remote_post($webhook_url, $args);
+	wp_remote_post( $webhook_url, $args );
 }
+
+add_filter('gfpdf_register_templates', function($templates) {
+	$templates[] = plugin_dir_path(__FILE__) . 'pdf-templates/event-ticket/config.php';
+	return $templates;
+});
+
+
---
+++ pdf-templates/event-ticket.php
@@ -0,0 +1,19 @@
+<?php
+/** @var array $entry */
+/** @var array $form */
+/*
+* Gravity PDF Custom Template - Event Ticket
+ * Name: Event Ticket Template
+ * Description: PDF ticket including event details, Logo, QR code
+ */
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+// Get entry data
+$event_name       = rgar($entry, '3'); // Event Selection Field ID
+$registrant_name  = rgar($entry, '1.3') . ' ' . rgar($entry, '1.6'); // First + Last Name
+$attendees        = rgar($entry, '7'); // Number of Attendees
+$addons           = rgar($entry, '8'); // Add-ons field
+$entry_url        = GFCommon::get_base_url() . '?entry=' . $entry['id']; // URL for QR code
+?>
+
